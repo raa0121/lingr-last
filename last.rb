d2f1df4db3647e68d9031f0377bf9bee70b685c7
raa0121 <raa0121@gmail.com>
2013-01-25 17:35:28 +0900

    module Skeleton

---
+++ last.rb
@@ -0,0 +1,30 @@
+# -*- coding : utf8 -*-
+
+require 'json'
+
+module LingrLast
+  class Member
+  
+  end
+  
+  class Bot
+
+  end
+  
+  class Room
+
+  end
+
+  class Message
+
+  end
+
+  class APIError
+
+  end
+
+  class Connection
+
+  end
+
+end
