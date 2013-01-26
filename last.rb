cade3c838551d03ecf6ee34e9b56ea9393d70bf7
raa0121 <raa0121@gmail.com>
2013-01-26 11:47:49 +0900

    煮詰まってきたので一度push

--- last.rb
+++ last.rb
@@ -1,3 +1,11 @@
-# -*- coding : utf8 -*-
+# -*- coding : utf-8 -*-
 
+$LOAD_PATH << File.dirname(__FILE__)
+
+require 'lingr'
+require 'highline'
+
+id = HighLine.new.ask('ID: ')
+pw = HighLine.new.ask('Password: '){|q| q.echo = '*'}
+last = Lingr::Connection.new(id, pw, 30)
 
